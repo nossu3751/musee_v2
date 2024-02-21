1e615d7be9ecf1a3b225db88b8088dc0490a3b30
Young Suh <yahoyoungho@gmail.com>
2024-02-20 22:50:56 -0800

    fixing warning on models.py for get_db

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 # HANDLING DB CONNECTIONS
 import aiosqlite
+from typing import AsyncGenerator
 from fastapi import Depends
 
 DATABASE_URL = "sqlite_async:///./test.db"
 
-async def get_db() -> aiosqlite.Connection:
+# indicating that get_db is an async generator function that yields aiosqlite.
+#Connection objects and eventually returns None when it's done.
+async def get_db() -> AsyncGenerator[aiosqlite.Connection, None]:
     async with aiosqlite.connect(DATABASE_URL) as db:
         yield db
 
